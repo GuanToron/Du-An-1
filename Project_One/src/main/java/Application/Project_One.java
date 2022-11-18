ee7574c831a8ee08d4409387b5dd1de9474b16fd
Sh4anT0r0n <xtruongtdo2003@outlook.com>
2022-11-18 14:44:00 +0700

    First Commit

---
+++ Project_One/src/main/java/Application/Project_One.java
@@ -0,0 +1,14 @@
+
+
+package Application;
+
+/**
+ *
+ * @author Xuan Truong
+ */
+public class Project_One {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
